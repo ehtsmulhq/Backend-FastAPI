15eebb85b29590b01685c5fd72a412d0851fe7ac
Ehatasamul Haque Adib <111741030+ehtsmulhq@users.noreply.github.com>
2025-03-30 00:06:29 +0600

    Enhance FastAPI app configuration with title and description, and add tags to book router

--- Basic CURD App/src/__init__.py
+++ Basic CURD App/src/__init__.py
@@ -4,10 +4,12 @@ from src.books.routers import book_router
 version="v1"
 
 app =FastAPI(
+    title="Book Management",
+    description="A REST API for a book review web service",
     
     version=version
 )
 
 
 
-app.include_router(book_router, prefix="/api/{version}/books")
+app.include_router(book_router, prefix="/api/{version}/books",tags=['books'])
